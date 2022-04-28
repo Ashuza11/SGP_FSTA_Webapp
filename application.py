e6122644bcad614f134e1f7cc60dd6513f514e6e
M ash <ashuza1411@gmail.com>
2022-04-29 01:01:26 +0200

    Style home page

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 # App configurations 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # DB configurations 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 ## Data base instance 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
 # DataBase Model 
